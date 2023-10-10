7d4014ae18c6633462ba38b21293d9f7db6c880a
SavindaJayasekara <thantrige32@gmail.com>
2023-10-10 22:41:35 +0530

    add components

--- assingment/JS/In-Memory-POS/controllers/PurchaseOrderController.js
+++ assingment/JS/In-Memory-POS/controllers/PurchaseOrderController.js
@@ -1,1 +1,38 @@
-//write all the order form validations here
+function loadCustomerId(){
+    for (const customer of customerDB) {
+        $('#selCusId').append(`<option>${customer.id}</option>`);
+    }
+}
+
+$('#selCusId').on('change',function (){
+    let id = $('#selCusId').val();
+    for (const customer of customerDB) {
+        if (customer.id == id){
+            $('#orderCusName').val(customer.name);
+            $('#orderCusAddres').val(customer.address);
+            $('#orderCusTp').val(customer.tp);
+        }
+    }
+});
+
+
+function loadAllItemId(){
+    for (const item of itemDB) {
+        $('#selItemId').append(`<option>${item.code}</option>`)
+    }
+}
+
+$('#selItemId').on('change',function (){
+    let id = $('#selItemId').val();
+    for (const item of itemDB) {
+        if (item.code == id){
+            $('#orderItemDesc').val(item.description);
+            $('#orderItemPrice').val(item.unitPrice);
+            $('#orderQty').val(item.qtyOnHand);
+        }
+    }
+});
+
+loadAllItemId();
+
+loadCustomerId();
