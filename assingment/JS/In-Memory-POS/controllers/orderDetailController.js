2b97846ee17711c746ee381a34705193141b5212
SavindaJayasekara <thantrige32@gmail.com>
2023-10-11 20:45:40 +0530

    add load all order details

---
+++ assingment/JS/In-Memory-POS/controllers/orderDetailController.js
@@ -0,0 +1,15 @@
+function loadAllOrderDetails(){
+    for (const object of orderDB) {
+        for (let i = 0; i < object.orderDetails.length; i++) {
+            $('#order-detail-body').append(`<tr>
+                <td>${object.oid}</td>
+                <td>${object.customerID}</td>
+                <td>${object.orderDetails[i].code}</td>
+                <td>${object.orderDetails[i].qty}</td>
+                <td>${object.date}</td>
+            </tr>`);
+        }
+    }
+}
+
+loadAllOrderDetails();
