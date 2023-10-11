f26099b9d99d6a3595e6c34a723037087308dbc9
SavindaJayasekara <thantrige32@gmail.com>
2023-10-11 09:45:48 +0530

    fix table row bug

--- assingment/JS/In-Memory-POS/controllers/PurchaseOrderController.js
+++ assingment/JS/In-Memory-POS/controllers/PurchaseOrderController.js
@@ -1,25 +1,48 @@
 let today = new Date().toISOString().slice(0, 10);
 
 $('#txtDate').css({
-    color:'green',
-    fontWeight:'500'
+    color: 'green',
+    fontWeight: '500'
 });
+
 $('#txtDate').val(today);
 
-$('#btnAddOrder').on('click',function (){
+$('#btnAddOrder').on('click', function () {
+
+    let price = parseFloat($('#orderItemPrice').val());
+
+    let total = price * parseInt($('#getQty').val());
+
+    let list = $(`#order-tbl-body > tr > td:nth-child(1)`);
+
+    for (const td of list) {
+        if ($(td).text() == $('#selItemId').val()){
+            let row  = $(td).parent();
+            $(row).remove();
+        }
+    }
+
+    $(`#order-tbl-body`).append(`<tr>
+        <td>${$('#selItemId').val()}</td>
+        <td>${$('#orderItemDesc').val()}</td>
+        <td>${$('#orderItemPrice').val()}</td>
+        <td>${$('#getQty').val()}</td>
+        <td>${total}</td>
+    </tr>`);
+
 
 });
 
-function loadCustomerId(){
+function loadCustomerId() {
     for (const customer of customerDB) {
         $('#selCusId').append(`<option>${customer.id}</option>`);
     }
 }
 
-$('#selCusId').on('change',function (){
+$('#selCusId').on('change', function () {
     let id = $('#selCusId').val();
     for (const customer of customerDB) {
-        if (customer.id == id){
+        if (customer.id == id) {
             $('#orderCusName').val(customer.name);
             $('#orderCusAddres').val(customer.address);
             $('#orderCusTp').val(customer.tp);
@@ -29,16 +52,16 @@ $('#selCusId').on('change',function (){
 });
 
 
-function loadAllItemId(){
+function loadAllItemId() {
     for (const item of itemDB) {
         $('#selItemId').append(`<option>${item.code}</option>`)
     }
 }
 
-$('#selItemId').on('change',function (){
+$('#selItemId').on('change', function () {
     let id = $('#selItemId').val();
     for (const item of itemDB) {
-        if (item.code == id){
+        if (item.code == id) {
             $('#orderItemDesc').val(item.description);
             $('#orderItemPrice').val(item.unitPrice);
             $('#orderQty').val(item.qtyOnHand);
